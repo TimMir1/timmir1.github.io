cb7ab0043862bc4364b1407be6661bb3add1d61f
TimMir1 <timyur0530@mail.ru>
2026-07-08 15:44:13 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#f0b429',
+        'gold-light': '#ffd166',
+        dark: '#1a1a1a',
+      },
+    },
+  },
+  plugins: [],
+}
